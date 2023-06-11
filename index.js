1c9d562e8643dd566ded8052924544e88417c8b7
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-11 16:05:24 +0100

    authentication

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import userRoutes from './routes/users.js'
 import restaurants from './routes/restaurants.js'
+import supabaseProvider from './supabase.js'
 import cors from 'cors'
 
 const app = express()
@@ -12,10 +13,29 @@ app.use(bodyParser.json())
 
 app.use('/users', userRoutes)
 app.use('/restaurants/', restaurants)
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/', (req, res) => {
   console.log('first')
   res.send('hello')
+})
+
+app.post('/login', async (req, res) => {
+  // console.log('first')
+  const { email, password } = req.body
+  try {
+    const { data, error } = await supabaseProvider.auth.signInWithPassword({
+      email,
+      password,
+    })
+    if (error) throw error
+    res.json({ data })
+    // res.send('all ok')
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+    console.log(email, password)
+  }
 })
 
 app.listen(PORT, () =>
